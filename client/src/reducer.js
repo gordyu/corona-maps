a7a1accdd98bb869597b2cf059ec88b4ae120692
Gordon Yu <gyunit@gmail.com>
2020-03-25 03:03:46 -0700

    create and style signout button and de-authentication logic

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -9,6 +9,12 @@ export default function reducer(state, { type, payload }) {
             return {
                 ...state,
                 isAuth: payload
+            };
+        case "SIGNOUT_USER":
+            return {
+                ...state,
+                isAuth: false,
+                currentUser: null
             }
         default:
             return state;

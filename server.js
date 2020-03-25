59103b3895135c885ed1ee7f0d3b8fbec376336b
Gordon Yu <gyunit@gmail.com>
2020-03-25 02:04:59 -0700

    Resolved authentication errors

--- server.js
+++ server.js
@@ -1,37 +1,34 @@
-const { ApolloServer } = require('apollo-server')
-const mongoose = require('mongoose')
-require('dotenv').config()
-
-const typeDefs = require('./typeDefs')
-const resolvers = require('./resolvers')
-const { findOrCreateUser } = require('./controllers/userController')
-
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('Connected to MongoDB Atlas!'))
-    .catch(err => console.error(err));
-
+const {ApolloServer} = require('apollo-server');
+const mongoose = require('mongoose');
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+require('dotenv').config();
+const { findOrCreateUser } = require('./controllers/userController');
+ 
+mongoose.connect(process.env.MONGO_URI, 
+                { useNewUrlParser: true,
+                useUnifiedTopology: true})
+.then( () => console.log("DB connected"))
+.catch( (err) => console.log(err));
+ 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({ req }) => {
-        let authToken = null
-        let currentUser = null
-        try {
-            authToken = req.headers.authorization
-            if (authToken) {
-                //find user in database or, if new user, create the user
-                currentUser = await findOrCreateUser(authToken)
-            }
-        } catch(err) {
-            console.error(`Unable to authenticate user with token ${authToken}`)
-        }
-        return { currentUser }
+  typeDefs,
+  resolvers,
+  context: async ({req}) => {
+    let authToken = null;
+    let currentUser = null;
+    try {
+      authToken = req.headers.authorization;
+      if ( authToken ) {
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch(err) {
+      console.log(`unable to auth user with token ${authToken}`);
     }
-})
-
-server.listen().then(({ url }) => {
-    console.log(`Server listening on ${url}`);
-    //visit localhost:4000/playground
+    return { currentUser };
+  }
+});
+ 
+server.listen().then(({url}) => {
+  console.log(`server listening in ${url}`);
 });
